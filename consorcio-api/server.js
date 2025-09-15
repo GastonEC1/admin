007bdf5d37a464dbdadbaa2b8be24a3e1b25415b
Gaston Carrizo <115049851+GastonEC1@users.noreply.github.com>
2025-09-15 13:23:39 +0000

    index4

--- consorcio-api/server.js
+++ consorcio-api/server.js
@@ -9,8 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
     origin: [
-        'https://refactored-xylophone-jv659gpjqq62jqr5-3000.app.github.dev', 
-        'http://localhost:3000'
+        'https://prueba-3-1-nmrz.onrender.com'
     ],
     optionsSuccessStatus: 200
 };
