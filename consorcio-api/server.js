98c3a18e18c2a0339c19b28eccc8cc4d9de83a34
GastonEC1 <gastoncarrizo123@gmail.com>
2025-09-16 13:12:58 +0000

    refresh4

--- consorcio-api/server.js
+++ consorcio-api/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet')
 const cors = require('cors');
+const path = require('path')
 require('dotenv').config();
 
 const app = express();
